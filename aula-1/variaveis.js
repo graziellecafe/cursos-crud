984cf59b28b187c53761847dd5de23aa11f7d38c
Graielle Café <graziellecafe@aluno.ufop.edu.br>
2023-07-11 11:54:39 -0300

    sessão 1: variáveis

---
+++ aula-1/variaveis.js
@@ -0,0 +1,35 @@
+var n1 = 10; 
+var n2 = 20; 
+
+console.log(n1+n2); // 30 
+
+// Como devs o que fazemos é LER código!
+// Contexto é extremamente importante! 
+
+const userFirstInputNumber = 10;
+const userSecondFInputNumber = 10;
+
+console.log(useFirsInput + userSecondFInput )
+
+
+// =================
+var inputDoUsuário
+// No Browser
+// - string 
+// - elemento do usu[ario ]
+const inputUsuario  = document.querySelector('input').value
+const $inputUsuario = document.querySelector('input'); 
+
+
+// Booleanos
+// <input value="" />
+document.querySelector('input').hasAttribute('value'); // true | false
+// has, is (são meus favoritos)
+if(hasSomething || isAnything) {}
+if(!hasSomething) {}
+
+// should
+if(shouldOpen) {}
+
+
+
